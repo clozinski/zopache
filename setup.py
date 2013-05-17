88a346a7008af4f7f41c8f05c5315070d105899e
Stephan Richter <stephan.richter@gmail.com>
2013-05-17 04:26:36 -0400

    Initial draft of the Zopache web app based on ZTFY.

---
+++ setup.py
@@ -0,0 +1,97 @@
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as file:
+        return file.read()
+
+setup(name='zopache.webapp',
+      version='0.1',
+      description='Zopache Web Application',
+      long_description=(
+        read('README.txt')
+        ),
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      keywords='zope bluebream ttw',
+      author='Christopher Lozinski',
+      author_email='lozinski@freerecruiting.com',
+      url='http://zopache.com',
+      license='ZPL 2.1',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'z3c.pt',
+        'z3c.template',
+        'z3c.testsetup',
+        'zope.app.apidoc',
+        'zope.app.appsetup',
+        'zope.app.basicskin',
+        'zope.app.broken',
+        'zope.app.catalog',
+        'zope.app.content',
+        'zope.app.component',
+        'zope.app.container',
+        'zope.app.debug',
+        'zope.app.dependable',
+        'zope.app.error',
+        'zope.app.file',
+        'zope.app.folder',
+        'zope.app.form',
+        'zope.app.generations',
+        'zope.app.i18n',
+        'zope.app.intid',
+        'zope.app.locales',
+        'zope.app.pagetemplate',
+        'zope.app.principalannotation',
+        'zope.app.publisher',
+        'zope.app.rotterdam',
+        'zope.app.schema',
+        'zope.app.security',
+        'zope.app.session',
+        'zope.app.testing',
+        'zope.app.wsgi',
+        'zope.app.zcmlfiles',
+        'zope.app.zopeappgenerations',
+        'zope.annotation',
+        'zope.browserresource',
+        'zope.catalog',
+        'zope.component',
+        'zope.contentprovider',
+        'zope.fanstatic',
+        'zope.formlib',
+        'zope.i18n',
+        'zope.intid',
+        'zope.keyreference',
+        'zope.login',
+        'zope.publisher',
+        'zope.securitypolicy',
+        'zope.sendmail',
+        'zope.testbrowser',
+        'ztfy.blog',
+        'ztfy.scheduler',
+        'ztfy.zmi',
+        ],
+      entry_points = """
+      [paste.app_factory]
+      main = zopache.webapp.startup:application_factory
+
+      [paste.global_paster_command]
+      shell = zopache.webapp.debug:Shell
+      """,
+      )
---
+++ src/zopache/webapp/startup.py
@@ -0,0 +1,40 @@
+##############################################################################
+#
+# Copyright (c) 2013 Christopher Lozinski (lozinski@freerecruiting.com)
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Application Startup."""
+import zope.app.wsgi
+
+def application_factory(global_conf):
+    zope_conf = global_conf['zope_conf']
+    app = zope.app.wsgi.getWSGIApplication(zope_conf)
+
+    def wrapper(environ, start_response):
+        vhost = ''
+        vhost_skin = environ.get('HTTP_X_VHM_SKIN')
+        if vhost_skin and \
+           (not environ.get('CONTENT_TYPE', '').startswith('application/json')) and \
+           (not environ.get('PATH_INFO', '').startswith('/++skin++')):
+            vhost = '/++skin++' + vhost_skin
+        url_scheme = environ.get('wsgi.url_scheme', 'http')
+        vhost_root = environ.get('HTTP_X_VHM_ROOT', '')
+        if (url_scheme == 'https') or (vhost_root and (vhost_root != '/')):
+            # Standard virtual host URL rewriting
+            # Also required for HTTPS connexions
+            vhost += '%s/++vh++%s:%s:%s/++' % (vhost_root,
+                                               url_scheme,
+                                               environ.get('SERVER_NAME', ''),
+                                               environ.get('SERVER_PORT', '80'))
+        environ['PATH_INFO'] = vhost + environ['PATH_INFO']
+        return app(environ, start_response)
+
+    return wrapper
